a02ee72ab389944846729bfb7b7800c6dc49ba9c
AkshatSachan <52987091+AkshatSachan@users.noreply.github.com>
2019-11-11 14:14:48 +0530

    Adding comments

--- Python-Calculator.py
+++ Python-Calculator.py
@@ -1,17 +1,21 @@
-import re
+'''
+A calculator in Python that calculates arithmetic equations typed in as a string using
+the eval() function
+Performs addition, subtraction, division and multiplication
+'''
+import re   #importing the regex module
 
 
 def perform_math(prev):
-    run = True
-    eq = input()
+    eq = input()    #enter 'quit' to exit the program
     if eq == "quit":
-        rum = False
+        pass
     else:
-        eq=re.sub('[A-Za-z,@,#,$,.?[]{}]','',eq)
-        if prev!=0:
-            prev=eval(str(prev)+eq)
+        eq = re.sub('[A-Za-z,@,#,$,.?[]{}]','',eq)
+        if prev != 0:
+            prev = eval(str(prev)+eq)
         else:
-            prev=eval(eq)
+            prev = eval(eq)
         print(prev)
         perform_math(prev)
 
